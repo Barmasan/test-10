8a9ec67d402ebff3500d66ca3efa37e9faa1305a
Barmasan <veselovskij.fedor@gmail.com>
2019-11-08 16:09:42 +0300

    Beta 0.1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ let el = document.querySelector('.choose__item-wrapper');
     function setTypeSection() {
         let chooseBG = document.querySelector('.choose-bg');
         let typeBG = document.querySelector('.type-bg');
+        let chooseContentToHide = document.getElementById('chooseContentToHide');
+        let chooseHeader = document.querySelector('.choose__header');
+        let chooseLabels = document.querySelector('.choose__labels');
+        let chooseContainer = document.getElementById('chooseContainer');
+        let typeNav = document.getElementById('typeNav');
+
         chooseBG.classList.add('unsetImage');
         typeBG.classList.add('setImage');
+        chooseHeader.classList.add('hide-choose-animation');
+        chooseContentToHide.classList.add('hide-choose-animation');
+        chooseLabels.classList.add('hide-choose-txt-animation');
+
+        setTimeout("chooseContainer.classList.add('disp-none');", 1500);
+        setTimeout("typeNav.classList.remove('disp-none');", 1500);
+        setTimeout("typeNav.classList.add('fade-in-animation');", 1500)
+            +
+            "typeNav.classList.remove('disp-none');" +
+             , 1500);
+
     }
+
